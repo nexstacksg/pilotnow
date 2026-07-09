c2a19966e3f27374daa65d104b74a60c7db98142
SiSiLyar <sisi@nexstack.sg>
2026-07-09 22:36:32 +0630

    ui latest design

---
+++ apps/web/app/[[...slug]]/page.tsx
@@ -0,0 +1,10 @@
+import { PilotNowLatest } from '../pilotnow-latest';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug?: string[] }>;
+}) {
+  const { slug = [] } = await params;
+  return <PilotNowLatest slug={slug} />;
+}
